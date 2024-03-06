56acb0b315d2d0212fb1776bf8bd657b7d174469
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-03-06 20:42:18 +0700

    Praktikum 2.2

---
+++ database/migrations/2024_03_06_130250_create_m_barang.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id')->index(); // indexing untuk ForeignKey
+            $table->string('barang_kode', 10)->unique();
+            $table->string('barang_nama', 100);
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
+            $table->timestamps();
+
+            //Mendefinisikan Foreign key
+            $table->foreign('kategori_id')->references('kategori_id')->on('m_kategori');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};
---
+++ database/migrations/2024_03_06_133545_create_t_penjualan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('user_id')->index(); // indexing untuk foreign key
+            $table->string('pembeli', 50)->unique();
+            $table->string('penjualan_kode', 20);
+            $table->dateTime('penjualan_tanggal');
+            $table->timestamps();
+
+            //Mendefinisikan Foreign key
+            $table->foreign('user_id')->references('user_id')->on('m_user');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan');
+    }
+};
