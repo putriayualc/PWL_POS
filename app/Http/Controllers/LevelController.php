32607d2f6c499f6bac1a9f6521d84de23e7bd141
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-03-06 23:08:37 +0700

    Praktikum 4

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LevelController extends Controller
+{
+   public function index(){
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)',['CUS', 'Pelanggan', now()]);
+        // return 'Insert data baru berhasil';
+
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?' , ['Customer', 'CUS']);
+        // return 'update data berhasil. Jumlah daya yang diupdate: ' . $row.' baris';
+
+        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row. ' baris';
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
+
+   }
+}
---
+++ resources/views/level.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Data Level Pengguna</title>
+    </head>
+    <body>
+        <h1>Data Level Pengguna</h1>
+        <table border="1" cellpading="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Kode Level</th>
+                <th>Nama Level</th>
+            </tr>
+            @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level_kode }}</td>
+                <td>{{ $d->level_nama }}</td>
+            </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
