d156cd488e824907182e07d425dfc94f5b6e5d42
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-03-07 13:01:01 +0700

    Praktikum 6

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(){
+        //tambah data user dengan Eloquent MOdel
+        $data = [
+            'nama' => 'Pelanggan Pertama'
+            // 'username' => 'customer-1',
+            // 'nama' => 'Pelanggan',
+            // 'password' => Hash::make('12345'),
+            // 'level_id' => 4
+        ];
+        // UserModel::insert($data); //tambahkan data ke tabel m_user
+        UserModel::where('username','customer-1')->update($data);
+
+        //coba akses model UserModel
+        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
+}
