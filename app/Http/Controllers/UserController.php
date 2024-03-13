68b6f4f0ec234160cc2d5f829a780141ce2d8ded
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-03-13 10:46:42 +0700

    Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,16 +24,20 @@ class UserController extends Controller
         // $user = UserModel::all(); //ambil semua data dari tabel m_user
         // return view('user', ['data' => $user]);
 
-        //Jobsheet 4
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        //JOBSHEET 4
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+        
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id',1);
+        $user = UserModel::findOr(20, ['username','nama'], function (){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
