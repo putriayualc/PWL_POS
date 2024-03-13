703adb0c43d005fbc758332806c515252e46f564
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-03-13 20:33:53 +0700

    Praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,8 +41,27 @@ class UserController extends Controller
 
         // $user = UserModel::findOrFail(1);
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        $user = UserModel::where('level_id', 2)->count();
+        // $user = UserModel::where('level_id', 2)->count();
         // dd($user);
+
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );
+
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+        $user -> save();
         return view('user', ['data' => $user]);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,6 +12,6 @@ class UserModel extends Model
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['level_id','username','nama'];
+    protected $fillable = ['level_id','username','nama','password'];
 
 }
