b6a58e4bbcebd5630a44be814edd1f7aefad72fc
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-05-02 00:16:45 +0700

    JS 10 - Prak.3

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class LogoutController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        //remove token
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if ($removeToken) {
+            //return response JSON
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Berhasil',
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::post('/login',App\Http\Controllers\Api\LoginController::class)->name('log
 Route::middleware('auth:api')->get('/user',function(Request $request){
     return $request->user();
 });
+Route::post('/logout',App\Http\Controllers\Api\LogoutController::class)->name('logout');
