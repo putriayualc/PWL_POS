e34bcbfd91fc01242ab916a2a4c1f4173c248311
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-05-02 00:50:15 +0700

    JS 10 - Prak.4

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,10 @@ Route::middleware('auth:api')->get('/user',function(Request $request){
     return $request->user();
 });
 Route::post('/logout',App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+//level
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
